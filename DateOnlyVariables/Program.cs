9703f193e629629f45cdf08a5b5608efbe3c33a6
Danny Warren <danny@theforbiddenarts.com>
2022-03-23 00:05:49 -0400

    code upload

--- DateOnlyVariables/Program.cs
+++ DateOnlyVariables/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+DateTime today = DateTime.Now;
+
+DateOnly birthday = DateOnly.Parse("6/11/1998");
+
+Console.WriteLine(birthday.ToString("MM/dd/yyyy"));
+
+
+Console.WriteLine($"Today full format: {today}");
+Console.WriteLine($"Today just dat {today.Date}");
+Console.WriteLine($"Birthday full format: {birthday}");
